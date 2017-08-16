5dca1f1ac1f06db9e7718185c63f721424571ddc
jimbauserman <jim.bauserman@gmail.com>
2017-08-16 18:34:41 -0400

    Generate ratings
    Fixes input.py to correctly build training dataset based on selected games and user sample

--- Scripts/Input.py
+++ Scripts/Input.py
@@ -1,18 +1,19 @@
-import itertools
 import pandas as pd
 import numpy as np
 
-ratings = pd.DataFrame()
+games = pd.read_csv('C://Python27//gameslist.csv')
+users = pd.read_csv('C://Python27//sample_userlist.csv')
 
-folder = 'C://python27/'
+game_set = set(games.values)
+user_set = set(users.values)
+
+ratings = pd.DataFrame(np.nan, index = users.values, columns = games.values)
+
 for i in range(1,50):
-    path  = folder + 'users' + str(i) + '.csv'
+    path  = 'C://python27/users' + str(i) + '.csv'
     current = pd.read_csv(path)
-    users = current['user'].unique()
-    for user in users:
-        curr_user = current.loc[current['user'] == user].dropna(subset = ['rating'])
-        curr_ratings = dict(itertools.izip(curr_user['gameid'], curr_user['rating']))
-        curr_ratings['user'] = user
-        ratings = ratings.append(curr_ratings, ignore_index = True)
-ratings.set_index('user', inplace = True)
-ratings.to_csv(folder+'Input.csv',header=True,index=False,encoding='utf-8')
+    for i, row in current.iterrows():
+        if row['user'] in user_set and row['gameid'] in game_set:
+            ratings.loc[ratings.index == row['user'], ratings.columns == row['gameid']] = row['rating']
+
+ratings.to_csv('C://python27/Input.csv',header=True,index=False,encoding='utf-8')
